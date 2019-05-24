dd7f2037b36a80019364bbb42c6d5b9ebd4eec9b
wmarnvind <wm@arnvind.com>
2019-05-24 22:05:10 +0200

    absolut impossible like Googles

--- assets/js/impossibleAI.js
+++ assets/js/impossibleAI.js
@@ -1,21 +1,31 @@
 function impossibleAI(){
   turn *= -1;
-  max;
-  maxval = -1;
-  board = deepCopy(map);
+  let max;
+  let maxval = -1000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+  let board = deepCopy(map);
+  let results = [];
+  for(let i = 0; i < 3; i++){
+    results.push([]);
+    for(let j = 0; j < 3; j++){
+      results[i].push(null);
+    }
+  }
   for(let i = 0; i < 3; i++){
     for(let j = 0; j < 3; j++){
+      let temp = -1;
       if(board[i][j] == 0){
         const tempBoard = deepCopy(board)
         tempBoard[i][j] = turn;
-        const temp = getScore(tempBoard, turn, turn, 1);
+        temp = getScore(tempBoard, turn * -1, turn, 1);
         if(temp > maxval){
           maxval = temp;
           max = [i, j];
         }
       }
+      results[j][i] = temp;
     }
   }
+  console.table(results);
   if(map){
       map[max[0]][max[1]] = turn;
   }
@@ -30,16 +40,19 @@ function deepCopy(arr){
 }
 
 function getScore(board, active, ideal, layer){
-  //console.log(board);
-  if(checkForWinnerAI(board, ideal)) return 1 / layer;
-  if(checkForWinnerAI(board, ideal*-1)) return -1 / layer;
-  sum = 0;
+  if(checkForWinnerAI(board, ideal)) return 1/(layer*10); //Math.pow(2, 1/layer);
+  if(checkForWinnerAI(board, ideal*-1)) return -1/(layer/1000);
+  let sum = 0;
   for(let i = 0; i < 3; i++){
     for(let j = 0; j < 3; j++){
       if(board[i][j] == 0){
-        const newboard = deepCopy(board);
+        let newboard = deepCopy(board);
         newboard[i][j] = active;
-        sum += getScore(newboard, active * -1, ideal, layer*10);
+        let temp = getScore(newboard, active * -1, ideal, layer * 10);
+        temp = temp * temp * (temp/Math.abs(temp));
+        if(temp){
+          sum += temp;
+        }
       }
     }
   }
